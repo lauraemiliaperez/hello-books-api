e393984efd8261922a8a051554f6951ca529e941
Laura <lauraemiliaperez@outlook.com>
2023-05-09 08:36:31 -0700

    __init__

--- app/__init__.py
+++ app/__init__.py
@@ -13,16 +13,16 @@ load_dotenv()
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    if not test_config:
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    else:
-        app.config["TESTING"] = True
-        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_TEST_DATABASE_URI')
     
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    # else:
+    #     app.config["TESTING"] = True
+    #     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            'RENDER_DATABASE_URI')
+
     
     db.init_app(app)
     migrate.init_app(app, db)
